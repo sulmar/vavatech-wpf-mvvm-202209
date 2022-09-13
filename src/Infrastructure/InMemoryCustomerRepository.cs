a5356807d9cd0f00e2864e40a57bfcde2d5c9492
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2022-09-13 14:41:03 +0200

    Refaktoring - utworzenie repozytorium

---
+++ src/Infrastructure/InMemoryCustomerRepository.cs
@@ -0,0 +1,25 @@
+﻿using Domain.Models;
+using Domain.Repositories;
+
+namespace Infrastructure
+{
+    public class InMemoryCustomerRepository : ICustomerRepository
+    {
+        private readonly ICollection<Customer> customers;
+
+        public InMemoryCustomerRepository()
+        {
+            customers = new List<Customer>
+            {
+                new Customer { Id = 1, FirstName = "John", LastName = "Smith", Phone = "555-111-222", Avatar = "https://api.lorem.space/image/face?w=150&h=150" },
+                new Customer { Id = 2, FirstName = "Ann", LastName = "Smith", Phone = "555-222-333", Avatar = "https://api.lorem.space/image/face?w=150&h=150" },
+                new Customer { Id = 3, FirstName = "Bob", LastName = "Smith", Phone = "555-333-444", Avatar = "https://api.lorem.space/image/face?w=150&h=150" },
+            };
+        }
+
+        public ICollection<Customer> Get()
+        {
+            return customers;
+        }
+    }
+}
--- src/ViewModels/CustomersViewModel.cs
+++ src/ViewModels/CustomersViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using Domain.Models;
+using Domain.Repositories;
+using Infrastructure;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,13 +14,14 @@ namespace ViewModels
         public ICollection<Customer> Customers { get; set; }
 
         public CustomersViewModel()
+            : this(new InMemoryCustomerRepository())
         {
-            Customers = new List<Customer>
-            {
-                new Customer { Id = 1, FirstName = "John", LastName = "Smith", Phone = "555-111-222", Avatar = "https://api.lorem.space/image/face?w=150&h=150" },
-                new Customer { Id = 2, FirstName = "Ann", LastName = "Smith", Phone = "555-222-333", Avatar = "https://api.lorem.space/image/face?w=150&h=150" },
-                new Customer { Id = 3, FirstName = "Bob", LastName = "Smith", Phone = "555-333-444", Avatar = "https://api.lorem.space/image/face?w=150&h=150" },
-            };
+
+        }
+
+        public CustomersViewModel(ICustomerRepository customerRepository)
+        {
+            Customers = customerRepository.Get();
         }
 
     }
