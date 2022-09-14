94d749b3259353ce904b8273c3e703d6d2e96632
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2022-09-14 12:45:04 +0200

    Dodanie kontrolek

---
+++ src/Fundamentals/04_Controls/ControlsView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Fundamentals._04_Controls
+{
+    /// <summary>
+    /// Interaction logic for ControlsView.xaml
+    /// </summary>
+    public partial class ControlsView : Window
+    {
+        public ControlsView()
+        {
+            InitializeComponent();
+        }
+    }
+}
