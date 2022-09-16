6f9f69140379aca92b206160497cf976db901255
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2022-09-16 14:19:52 +0200

    Przykład Shapes

---
+++ src/Fundamentals/05_Graphics/ShapesView.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Fundamentals._05_Graphics
+{
+    /// <summary>
+    /// Interaction logic for ShapesView.xaml
+    /// </summary>
+    public partial class ShapesView : Window
+    {
+        public ShapesView()
+        {
+            InitializeComponent();
+
+            
+        }
+    }
+}
