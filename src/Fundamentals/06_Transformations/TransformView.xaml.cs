2a4ee7ae0fb85c1521ca84a9c233cfc8525d62da
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2022-09-16 14:57:57 +0200

    Transformacje

---
+++ src/Fundamentals/06_Transformations/TransformView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Fundamentals._06_Transformations
+{
+    /// <summary>
+    /// Interaction logic for TransformView.xaml
+    /// </summary>
+    public partial class TransformView : Window
+    {
+        public TransformView()
+        {
+            InitializeComponent();
+        }
+    }
+}
